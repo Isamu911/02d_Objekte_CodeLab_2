0fffc3ff8839ae8e1ede4f1a8a098569cc8c1ecd
Isamu911 <mwasielewski@gmx.de>
2021-12-03 14:49:49 +0100

    3

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -3,7 +3,50 @@ package com.cc.java;
 public class App {
 
 	public static void main(String[] args) {
-		
+		// **Name/Vorname/Funktion/Eintrittsjahr**
+		Mitarbeiter worker1 = new Mitarbeiter("Duck", "Donald", "Schleifer", 2001);
+	
+	
+        output("Nachname: " + worker1.getLastName());
+        output("Vorname: " + worker1.getFirstName());
+        output("Funktion: " + worker1.getFunction());
+        output("Eintrittsjahr: " + worker1.getBeginningYear());
+
+		output("----------------");
+
+		Mitarbeiter worker2 = new Mitarbeiter("Duck", "Dagobert", "Chef", 1989);
+	
+	
+        output("Nachname: " + worker2.getLastName());
+        output("Vorname: " + worker2.getFirstName());
+        output("Funktion: " + worker2.getFunction());
+        output("Eintrittsjahr: " + worker2.getBeginningYear());
+
+		output("------------------");
+
+
+		Mitarbeiter worker3 = new Mitarbeiter("Düsentrieb", "Daniel", "Entwickler", 1990);
+	
+	
+        output("Nachname: " + worker3.getLastName());
+        output("Vorname: " + worker3.getFirstName());
+        output("Funktion: " + worker3.getFunction());
+        output("Eintrittsjahr: " + worker3.getBeginningYear());
+
+
+		output("------------------");
+
+		Mitarbeiter worker4 = new Mitarbeiter("Duck", "Daisy", "Ärztin", 2001);
+	
+	
+        output("Nachname: " + worker4.getLastName());
+        output("Vorname: " + worker4.getFirstName());
+        output("Funktion: " + worker4.getFunction());
+        output("Eintrittsjahr: " + worker4.getBeginningYear());
+
+		output("------------------");
+
+
 	}
 
 	private static void output(String outStr) {
--- src/com/cc/java/Mitarbeiter.java
+++ src/com/cc/java/Mitarbeiter.java
@@ -1,5 +1,37 @@
 package com.cc.java;
 
 public class Mitarbeiter {
+    private String lastName;
+    private String firstName;
+    private String function;
+    private int beginningYear;
+
+    public Mitarbeiter(String lastName, String firstName, String function, int beginningYear){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.function = function;
+        this.beginningYear = beginningYear;
+
+    }
     
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public int getBeginningYear() {
+        return beginningYear;
+    }
+
+    
+
+
 }
